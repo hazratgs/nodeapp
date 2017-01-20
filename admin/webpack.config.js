2d399f18691fadba06bb093128eae2a83c2a4aad
febox <febox@Mac-Pro-febox.local>
2017-01-20 21:28:26 +0300

    React-redux для admin

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -1,5 +1,7 @@
 var path = require('path')
 var webpack = require('webpack')
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
@@ -15,7 +17,7 @@ module.exports = {
     },
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
     ],
     module: {
         preLoaders: [
@@ -35,7 +37,14 @@ module.exports = {
                 ],
                 test: /\.js$/,
                 plugins: ['transform-runtime'],
+            },
+            {
+                test:   /\.css$/,
+                loader: "style-loader!css-loader!postcss-loader"
             }
         ]
+    },
+    postcss: function () {
+        return [autoprefixer, precss];
     }
 }
